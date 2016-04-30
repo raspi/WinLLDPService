486c742a14f3ac45600803a914cf2f00ff3f54f2
Pekka Järvinen <pekka.jarvinen@gmail.com>
2016-04-30 19:14:20 +0300

    Set .msi file as a variable

--- WinLLDPService/build/build.bat
+++ WinLLDPService/build/build.bat
@@ -1,8 +1,11 @@
 @echo off
+SET MSINAME="WinLLDPService-installer.msi"
 echo [x] Applying paths..
 if not exist "paths.bat" goto error
 
 call paths.bat
+
+if exist %MSINAME% del %MSINAME%
 
 echo.
 echo [x] Generating WIX Object..
@@ -10,10 +13,12 @@ candle -ext WiXNetFxExtension -ext WixUtilExtension installer.wxs || goto error
 
 echo.
 echo [x] Generating MSI file..
-light -ext WixUIExtension -ext WiXNetFxExtension -out WinLLDPService-installer.msi installer.wixobj || goto error
+light -ext WixUIExtension -ext WiXNetFxExtension -out %MSINAME% installer.wixobj || goto error
 
 del *.wixobj
 del *.wixpdb
+
+if not exist %MSINAME% goto error
 
 echo.
 echo All done. Press any key.
