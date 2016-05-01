8e2e1133bd7802d7ecde97aefa27a6286f3deea3
Pekka Järvinen <pekka.jarvinen@gmail.com>
2016-05-01 19:39:32 +0300

    Added redirecting of various build tools to log files

--- WinLLDPService/build/build.ps1
+++ WinLLDPService/build/build.ps1
@@ -9,22 +9,28 @@ $version_script = "$pwd/generate_version.ps1"
 
 # Version include file
 $version_file = "$pwd/version.wxi"
-$process = Start-Process powershell -Wait -PassThru -NoNewWindow -ArgumentList "-File $version_script $version_file"
+$stdErrLog = "$pwd/vergen-stderr.log"
+$stdOutLog = "$pwd/vergen-stdout.log"
+$process = Start-Process powershell -Wait -PassThru -NoNewWindow -ArgumentList "-File $version_script $version_file" -RedirectStandardOutput $stdOutLog -RedirectStandardError $stdErrLog
 
-if(![System.IO.File]::Exists($version_file)) {
+if(![System.IO.File]::Exists("$version_file")) {
   Write-Host ("ERROR: Version file '{0}' not found." -f $version_file)
   Exit 1
 }
 
 # Set environmental variables for WiX
-Write-Host ("Calling '{0}'.." -f $paths_file)
 $paths_file = "$pwd/paths.ps1"
-if(![System.IO.File]::Exists($paths_file)) {
+Write-Host ("Calling '{0}' to set environmental variables.." -f $paths_file)
+
+if(![System.IO.File]::Exists("$paths_file")) {
   Write-Host ("ERROR: Paths file '{0}' not found." -f $paths_file)
-  exit 1
+  $null = [System.Console]::ReadKey()
+  Exit 1
 }
 
 & $paths_file
+
+Write-Host ""
 
 # Remove old installer file if it exists
 if([System.IO.File]::Exists("$output_installer_file")) {
@@ -32,11 +38,17 @@ if([System.IO.File]::Exists("$output_installer_file")) {
 }
 
 Write-Host "Running WiX candle.."
-$process = Start-Process candle -Wait -PassThru -NoNewWindow -ArgumentList '-v -ext WiXNetFxExtension -ext WixUtilExtension installer.wxs'
+$stdErrLog = "$pwd/candle-stderr.log"
+$stdOutLog = "$pwd/candle-stdout.log"
+$process = Start-Process candle -Wait -PassThru -NoNewWindow -ArgumentList '-v -ext WiXNetFxExtension -ext WixUtilExtension installer.wxs' -RedirectStandardOutput $stdOutLog -RedirectStandardError $stdErrLog
 if($process.ExitCode -ne 0) {
   Write-Host "ERROR: running WiX candle failed."
-  Write-Host ($process.StandardOutput.ReadToEnd())
-  Write-Host ($process.StandardError.ReadToEnd())
+  
+  Get-Content $stdOutLog
+  Get-Content $stdErrLog  
+  
+  $null = [System.Console]::ReadKey()
+  
   Exit 1
 }
 
@@ -44,17 +56,18 @@ Write-Host ("-" * 40)
 Write-Host ""
 
 Write-Host "Running WiX light.."
-$process = Start-Process light -Wait -PassThru -NoNewWindow -ArgumentList "-v -ext WixUIExtension -ext WiXNetFxExtension -out $output_installer_file installer.wixobj"
+$stdErrLog = "$pwd/light-stderr.log"
+$stdOutLog = "$pwd/light-stdout.log"
+$process = Start-Process light -Wait -PassThru -NoNewWindow -ArgumentList "-v -ext WixUIExtension -ext WiXNetFxExtension -ext WixUtilExtension -out $output_installer_file installer.wixobj" -RedirectStandardOutput $stdOutLog -RedirectStandardError $stdErrLog
 if($process.ExitCode -ne 0) {
   Write-Host "ERROR: running WiX light failed."
-  Write-Host ($process.StandardOutput.ReadToEnd())
-  Write-Host ($process.StandardError.ReadToEnd())
+  
+  Get-Content $stdOutLog
+  Get-Content $stdErrLog  
+  
+  $null = [System.Console]::ReadKey()
+  
   Exit 1
-}
-
-# Remove generated version include file
-if([System.IO.File]::Exists($version_file)) {
-  Remove-Item $version_file
 }
 
 Write-Host ("-" * 40)
@@ -64,13 +77,22 @@ Write-Host ""
 Remove-Item "$pwd/*.wixobj"
 Remove-Item "$pwd/*.wixpdb"
 
-if(![System.IO.File]::Exists($output_installer_file)) {
+# Remove generated version include file
+if([System.IO.File]::Exists("$version_file")) {
+  Remove-Item "$version_file"
+}
+
+
+if(![System.IO.File]::Exists("$output_installer_file")) {
   Write-Host ("ERROR: .msi installer '{0}' not found." -f $output_installer_file)
+  $null = [System.Console]::ReadKey()
   exit 1
 }
 
 Write-Host "Build complete!"
 Write-Host ""
+
+Remove-Item "$pwd/*.log"
 
 Write-Host "Press any key to continue..."
 $null = [System.Console]::ReadKey()
