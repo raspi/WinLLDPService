807c34d850d1c6268cf43be5acde3759497baa02
Pekka Järvinen <pekka.jarvinen@gmail.com>
2017-07-12 20:21:57 +0300

    Description

--- WinLLDPService/LLDPBase/Properties/AssemblyInfo.cs
+++ WinLLDPService/LLDPBase/Properties/AssemblyInfo.cs
@@ -7,6 +7,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("LLDPBase")]
 [assembly: AssemblyProduct("LLDPBase")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Base for WinLLDPService")]
 
 [assembly: Guid("57b661ac-c800-4e00-be9d-52ce58e1f0f1")]
