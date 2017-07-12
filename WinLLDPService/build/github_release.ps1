1c5a4f0acae70e944cbf55ce0f9da399eeaf1678
Pekka Järvinen <pekka.jarvinen@gmail.com>
2017-07-13 00:33:23 +0300

    refactor

--- WinLLDPService/build/github_release.ps1
+++ WinLLDPService/build/github_release.ps1
@@ -8,16 +8,13 @@ param (
 
 Push-Location "$PSScriptRoot"
 
-if (-not (Test-Path Env:ghapikey)) {
-	Write-Output "Environmental variable 'ghapikey' is not set."
-	Write-Output "Run:"
-	Write-Output '$Env:ghapikey = "<your GitHub API KEY>"'
-	Exit 1
-}
-
 $msifile = Resolve-Path "$msifile"
 
-# Set environmental variables for WiX
+if(![System.IO.File]::Exists("$msifile")) {
+  Write-Output ("File '{0}' not found" -f $msifile)
+  Exit 1
+} 
+
 # Set environmental variables for WiX location
 $paths_file = "$pwd/paths.ps1"
 
@@ -29,6 +26,7 @@ if([System.IO.File]::Exists("$paths_file")) {
 }
 
 # Register WiX MSI file information DLL
+Write-Output "Loading DLL.."
 Add-Type -Path "$Env:WIXPATH\Microsoft.Deployment.WindowsInstaller.dll"
 
 # Get version from MSI file
@@ -40,18 +38,25 @@ $db.Close()
 Write-Output "Version: $version"
 Write-Output ""
 
+$ghauth = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes($Env:ghapikey + ":x-oauth-basic"));
+$ghuri = "https://api.github.com/repos/raspi/WinLLDPService/releases"
+
 $releaseParams = @{
-   Uri = "https://api.github.com/repos/raspi/WinLLDPService/releases";
+   Uri = $ghuri;
    Method = 'GET';
    Headers = @{
-     Authorization = 'Basic ' + [Convert]::ToBase64String(
-     [Text.Encoding]::ASCII.GetBytes($Env:ghapikey + ":x-oauth-basic"));
+     Authorization = $ghauth;
    }
    ContentType = 'application/json';
 }
 
 Write-Output "Getting old release on github.."
-$result = Invoke-RestMethod @releaseParams
+try {
+  $result = Invoke-RestMethod @releaseParams
+} catch {
+  Write-Output ("REST ERROR: {0} {1}" -f $_.Exception.Response.StatusCode.value__, $_.Exception.Response.StatusDescription)
+  Exit 1
+}
 
 $id = 0
 $old_version_found = $false
@@ -60,6 +65,7 @@ $result | ForEach-Object {
 	if ($_.tag_name -eq "v$version") {
 		$id = $_.id
 		$old_version_found = $true
+		return
 	}
 }
 
@@ -76,29 +82,36 @@ if (!$old_version_found) {
 	}
 
 	$releaseParams = @{
-	   Uri = "https://api.github.com/repos/raspi/WinLLDPService/releases";
+	   Uri = $ghuri;
 	   Method = 'POST';
 	   Headers = @{
-		 Authorization = 'Basic ' + [Convert]::ToBase64String(
-		 [Text.Encoding]::ASCII.GetBytes($Env:ghapikey + ":x-oauth-basic"));
+		 Authorization = $ghauth;
 	   }
 	   ContentType = 'application/json';
 	   Body = (ConvertTo-Json $releaseData -Compress)
 	}
 
 	Write-Output "Making new release on github.."
-	$result = Invoke-RestMethod @releaseParams
-	$result
-
+	
+    try {
+      $result = Invoke-RestMethod @releaseParams
+    } catch {
+      Write-Output ("REST ERROR: {0} {1}" -f $_.Exception.Response.StatusCode.value__, $_.Exception.Response.StatusDescription)
+      Exit 1
+    }
+	
 	$id = $result.id
+} else {
+  Write-Output "Using old release on github.."
 }
 
 # Upload file
-$f = [System.IO.FileInfo] $msifile
+$f = [System.IO.FileInfo] "$msifile"
 $artifact = [string]::Format("{0}", $f.Name)
 
 Write-Output $result
 
+# Get upload URL from JSON
 $uploadUri = $result | Where-Object {$_.tag_name -eq "v$version"} | Select -ExpandProperty upload_url
 
 $uploadUri = [System.Uri]$uploadUri
@@ -110,16 +123,20 @@ $uploadParams = @{
    Uri = $uploadUri;
    Method = 'POST';
    Headers = @{
-     Authorization = 'Basic ' + [Convert]::ToBase64String(
-     [Text.Encoding]::ASCII.GetBytes($Env:ghapikey + ":x-oauth-basic"));
+     Authorization = $ghauth;
    }
    ContentType = 'application/octet-stream';
    InFile = $msifile
 }
 
 Write-Output "Uploading msi file.."
-$result = Invoke-RestMethod @uploadParams
-$result
+
+try {
+  $result = Invoke-RestMethod @uploadParams
+} catch {
+  Write-Output ("REST ERROR: {0} {1}" -f $_.Exception.Response.StatusCode.value__, $_.Exception.Response.StatusDescription)
+  Exit 1
+}
 
 Write-Output ""
 Write-Output "Now go to GitHub and release this version."
