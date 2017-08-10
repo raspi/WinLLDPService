c1e00db60be374fc89e4b07f24e2b1a3f254d73e
Pekka Järvinen <pekka.jarvinen@gmail.com>
2017-08-10 21:28:09 +0300

    company & copyright

--- WinLLDPService/Properties/AssemblyInfo.cs
+++ WinLLDPService/Properties/AssemblyInfo.cs
@@ -10,3 +10,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Small LLDP service")]
 
 [assembly: Guid("8804e4fe-e670-4543-bb72-eab0ddeb510e")]
+[assembly: AssemblyCompany("-")]
+[assembly: AssemblyCopyright("Pekka Järvinen")]
+
