2c9b118cb1a6c1e6877263dfec5251284bd63b24
Pekka Järvinen <pekka.jarvinen@gmail.com>
2018-02-03 00:04:52 +0200

    doc

--- WinLLDPService/LLDPBase/StaticInfo.cs
+++ WinLLDPService/LLDPBase/StaticInfo.cs
@@ -5,9 +5,24 @@
     /// </summary>
     public class StaticInfo
     {
+        /// <summary>
+        /// Gets or sets the machine name.
+        /// </summary>
         public string MachineName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the OS "friendly" name
+        /// </summary>
         public string OperatingSystemFriendlyName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the OS version
+        /// </summary>
         public string OperatingSystemVersion { get; set; }
+
+        /// <summary>
+        /// Gets or sets the service tag
+        /// </summary>
         public string ServiceTag { get; set; }
     }
 }
