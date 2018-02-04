23126936a391b1c53fa81e2e5efaf4ad32823531
Pekka Järvinen <pekka.jarvinen@gmail.com>
2018-02-04 21:14:19 +0200

    remove unnecessary

--- WinLLDPService/LLDPBase/NetworkInfo.cs
+++ WinLLDPService/LLDPBase/NetworkInfo.cs
@@ -5,43 +5,6 @@
     class NetworkInfo
     {
         /// <summary>
-        /// Convert network mask to CIDR notation
-        /// </summary>
-        /// <param name="ip">Network mask</param>
-        /// <returns></returns>
-        public static int GetCIDRFromIPMaskAddress(IPAddress ip)
-        {
-
-            byte[] bytes = ip.GetAddressBytes();
-
-            int cidrnet = 0;
-            bool zeroed = false;
-
-            foreach (var t in bytes)
-            {
-                for (int v = t; (v & 0xFF) != 0; v = v << 1)
-                {
-                    if (zeroed)
-                    {
-                        // invalid netmask
-                        return ~cidrnet;
-                    }
-
-                    if ((v & 0x80) == 0)
-                    {
-                        zeroed = true;
-                    }
-                    else
-                    {
-                        cidrnet++;
-                    }
-                }
-            }
-
-            return cidrnet;
-        }
-
-        /// <summary>
         /// Change for example adapter link speed into more human readable format
         /// </summary>
         /// <param name="size"></param>
@@ -49,7 +12,7 @@
         /// <returns></returns>
         public static string ReadableSize(double size, int unit = 0)
         {
-            string[] units = { "b", "Kb", "Mb", "Gb", "Tb", "Pb", "Eb", "Zb", "Yb" };
+            string[] units = { "b", "K", "M", "G", "T", "P", "E", "Z", "Y" };
 
             while (size >= 1000)
             {
@@ -59,7 +22,5 @@
 
             return string.Format("{0:G4}{1}", size, units[unit]);
         }
-
-
     }
 }
